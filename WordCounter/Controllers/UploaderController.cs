9390e060b056a519f84e4d82a4982353501c6491
Yakov <elsoruzo@gmail.com>
2020-08-21 22:59:28 +0300

    implementing parallelism naive(still slow)

--- WordCounter/Controllers/UploaderController.cs
+++ WordCounter/Controllers/UploaderController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -34,15 +37,31 @@ namespace WordCounter.Controllers
                  await Task.FromResult((string)null);
             }
     
+            var temp = new ConcurrentDictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
             using (var reader = new StreamReader(body.OpenReadStream()))
             {
-                text = await reader.ReadToEndAsync();
+                List<string> lines = new List<string>();
+                while (reader.Peek() >= 0)
+                {
+                    lines.Add(await reader.ReadLineAsync()); 
+                }
+                //text = await reader.ReadToEndAsync();
+                
+                Parallel.ForEach(
+                    lines,
+                    new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                    (line, state, index) =>
+                    {
+                        foreach (var word in line.Split(" "))
+                        {
+                            temp.AddOrUpdate("amounth", 1, (key, oldVal) => oldVal + 1);
+                        }
+                    }
+                );
             }
-            
-            int count = text.Split(' ').Length;
 
             var result = new StringBuilder("The amount of words in your file is: ")
-                .Append(count.ToString())
+                .Append(temp.First().Value.ToString())
                 .ToString();
             return Ok(result);
         }
