c5f3eb7ee3940279844007494c49e0fe30d2c165
Yakov <elsoruzo@gmail.com>
2020-08-22 00:41:33 +0300

    implementing parallelism and async

--- WordCounter/Controllers/UploaderController.cs
+++ WordCounter/Controllers/UploaderController.cs
@@ -11,59 +11,56 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WordCounter.Controllers
 {
-
     [Route("api/FileApi")]
     public class ApiController : Controller
     {
-
+        private static readonly char[] separators = {' '};
+        private static List<string> lines;
+        private static ConcurrentDictionary<string, int> freqeuncyDictionary;
 
         [HttpPost]
         [Route("Upload")]
-        public async Task<IActionResult> Post([FromRoute]Guid id, [FromForm]IFormFile body)
-        {
-            byte[] fileBytes;
-            string text;
-            using(var memoryStream = new MemoryStream())
-            {
-                await body.CopyToAsync(memoryStream);
-                fileBytes = memoryStream.ToArray();
-            }
+        public async Task<IActionResult> Post([FromRoute] Guid id, [FromForm] IFormFile body)
+        {   
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            lines = new List<string>();
 
-            var filename = body.FileName;
-            var contentType = body.ContentType;
-            
-            if (body == null || body.Length == 0)
+
+            using (var streamReader = new StreamReader(body.OpenReadStream()))
             {
-                 await Task.FromResult((string)null);
-            }
-    
-            var temp = new ConcurrentDictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
-            using (var reader = new StreamReader(body.OpenReadStream()))
-            {
-                List<string> lines = new List<string>();
-                while (reader.Peek() >= 0)
+                string line;
+                while ((line = await streamReader.ReadLineAsync()) != null)
                 {
-                    lines.Add(await reader.ReadLineAsync()); 
+                    lines.Add(line);
                 }
-                //text = await reader.ReadToEndAsync();
-                
-                Parallel.ForEach(
-                    lines,
-                    new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
-                    (line, state, index) =>
-                    {
-                        foreach (var word in line.Split(" "))
-                        {
-                            temp.AddOrUpdate("amounth", 1, (key, oldVal) => oldVal + 1);
-                        }
-                    }
-                );
             }
 
+            ConcurrentDictionary<string, int> test = GetFrequencyFromLines(lines);
+
+            stopwatch.Stop();
             var result = new StringBuilder("The amount of words in your file is: ")
-                .Append(temp.First().Value.ToString())
+                .Append(test.First().Value.ToString())
+                .Append(" Count on the server was completed after: ")
+                .Append(stopwatch.Elapsed.TotalSeconds)
                 .ToString();
             return Ok(result);
+        }
+
+        public static ConcurrentDictionary<string, int> GetFrequencyFromLines(List<string> lines)
+        {
+            freqeuncyDictionary = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(lines, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },line =>
+            {
+                var words = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    freqeuncyDictionary.AddOrUpdate("amount", 1, (key, oldValue) => oldValue + 1);
+                }
+            });
+
+            return freqeuncyDictionary;
         }
     }
 }
