f02d9aa15c7732ccce93182d4163f1ad2cc8fe60
Yakov <elsoruzo@gmail.com>
2020-08-23 21:45:48 +0300

    commenting to add an explenation

--- WordCounter/Controllers/UploaderController.cs
+++ WordCounter/Controllers/UploaderController.cs
@@ -26,13 +26,15 @@ namespace WordCounter.Controllers
         [Route("Upload")]
         public async Task<IActionResult> Post([FromRoute] Guid id, [FromForm] IFormFile body)
         {
-
+            // the stop watch is for debug purposes only this is why is not in a separate component 
             var stopwatch = new System.Diagnostics.Stopwatch();
             stopwatch.Start();
             var result = await _fileProcessingService.ProccessFile(body);
 
-
+            // the stop watch is for debug purposes only this is why is not in a separate component 
             stopwatch.Stop();
+            
+            // this StringBuilder is for debug purposes only this is why is not in a separate component 
             result = new StringBuilder(result)
                 .Append(" Count on the server was completed after: ")
                 .Append(stopwatch.Elapsed.TotalSeconds)
